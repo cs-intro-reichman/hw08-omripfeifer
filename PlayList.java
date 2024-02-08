dc7b3090cefa20d169f5dfadc9417cc63dd46c0c
omripfeifer <omri.pfeifer@post.runi.ac.il>
2024-02-08 07:48:23 +0200

    change 10

--- PlayList.java
+++ PlayList.java
@@ -219,12 +219,11 @@ class PlayList {
     public void sortedInPlace() {
        // Uses the selection sort algorithm,  
         // calling the minIndex method in each iteration.
-        for(int i=0;i<size-1;i++)
-        {
-         Track temp = this.tracks[minIndex(i)];
-         this.tracks[minIndex(i)]=this.tracks[i];
-         this.tracks[i]=temp;
+        for (int i = 0; i < size - 1; i++) {
+            int minIndex = minIndex(i);
+            Track temp = tracks[minIndex];
+            tracks[minIndex] = tracks[i];
+            tracks[i] = temp;
         }
-
-     }
+    }
 }
